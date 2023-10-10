d0b44df0f79605c98c853a2e86e394867453f327
Ali-HSNZ <Aliatraby@gmail.com>
2023-10-10 15:04:26 +0330

    style: change miniSwiper molecules - change swiper navigation button color when actived

--- src/components/UI/molecules/Swipers/MiniSwiper/MiniSwiper.tsx
+++ src/components/UI/molecules/Swipers/MiniSwiper/MiniSwiper.tsx
@@ -1,14 +1,13 @@
 'use client'
 
-import { type FC, useCallback, useRef } from 'react'
+import { type FC, useCallback, useRef, useState } from 'react'
 import Link from 'next/link'
 import { FreeMode } from 'swiper/modules'
-import { Swiper } from 'swiper/react'
+import { Swiper, type SwiperClass } from 'swiper/react'
 import { IconChevronLeft, IconChevronRight } from '@tabler/icons-react'
 
 import { Button } from '@atoms/Button'
 
-// Swiper Styles ---> Navigation
 import 'swiper/css/navigation'
 import 'swiper/css/free-mode'
 
@@ -18,38 +17,60 @@ const MiniSwiper: FC<IMiniSwiperProps> = ({ children, title = '', seeMoreLinkHre
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const swiperRef: any = useRef(null)
 
+    // Saved Swiper Slide Index ---> State
+    const [activeIndex, setActiveIndex] = useState(0)
+
+    // Update Slide Index ---> Handler ---> SetState
+    const handleSlideChange = useCallback((swiper: SwiperClass) => {
+        setActiveIndex(swiper.activeIndex)
+    }, [])
+
+    // change swiper slide ---> prev
     const handlePrev = useCallback(() => {
         if (!swiperRef.current) return
         swiperRef.current.swiper.slidePrev()
     }, [])
 
+    // change swiper slide ---> next
     const handleNext = useCallback(() => {
         if (!swiperRef.current) return
         swiperRef.current.swiper.slideNext()
     }, [])
 
+    // active button ---> className
+    const activeButtonClass = 'px-0 text-yellow-400 transition-all duration-300 bg-transparent'
+
+    // prev button ----> className
+    const inactiveButtonClass = 'px-0 text-gray-400 transition-all duration-300 bg-transparent'
+
+    // Prev Button State ----> boolean ---> Active | Inactive (className)
+    const prevButtonClass = activeIndex === 0 ? inactiveButtonClass : activeButtonClass
+
+    // Next Button State ----> boolean ---> Active | Inactive (className)
+    const nextButtonClass =
+        activeIndex === swiperRef.current?.swiper.slides.length - 1 ? inactiveButtonClass : activeButtonClass
+
     return (
         <div className='relative'>
-            {/* next slider */}
-
             <div className='flex justify-between items-center pr-6'>
+                {/* Section Title And Action Buttons */}
                 <div className='flex items-center gap-x-2'>
+                    {/* Section Title */}
                     <h1 className='font-bold text-xl text-gray-200'>{title}</h1>
+
+                    {/* Prev Button And Next Button */}
                     <div>
-                        <Button
-                            className='px-0 text-gray-400 hover:text-yellow-400 transition-all duration-300 bg-transparent'
-                            onClick={handlePrev}
-                        >
+                        {/* Prev Button */}
+                        <Button className={prevButtonClass} onClick={handlePrev}>
                             <IconChevronLeft />
                         </Button>
-                        <Button
-                            className='px-0 text-gray-400 hover:text-yellow-400 transition-all duration-300 bg-transparent'
-                            onClick={handleNext}
-                        >
+                        {/* Next Button */}
+                        <Button className={nextButtonClass} onClick={handleNext}>
                             <IconChevronRight />
                         </Button>
                     </div>
                 </div>
+                {/* Section Link ----> (See More | More Detail | ...) */}
                 <Link href={seeMoreLinkHref} className=' text-sm text-gray-400'>
                     {seeMoreLinkTitle}
                 </Link>
@@ -59,6 +80,7 @@ const MiniSwiper: FC<IMiniSwiperProps> = ({ children, title = '', seeMoreLinkHre
                 modules={[FreeMode]}
                 slidesPerView={'auto'}
                 freeMode
+                onSlideChange={handleSlideChange}
                 spaceBetween={30}
                 ref={swiperRef}
                 className='_miniSwiper mt-4 rounded-lg relative select-none'
